4d19a3c51712ae74c913222b7ff313176cd88869
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2023-11-29 05:47:19 -0800

    last

---
+++ _printd.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _printd - decimal print
+ * @r: len
+ * @s: num
+ * Return: len
+ */
+int _printd(int a, int b)
+{
+	int rem = 0, c = 0;
+	unsigned int n;
+	char num[11];
+
+	if (b < 0)
+	{
+		write(1, "-", 1);
+		a++;
+		n = -1 * b;
+	}
+	else
+		n = b;
+	if (n == 0)
+	{
+		write(1, "0", 1);
+		return (a + 1);
+	}
+	while (n > 0)
+	{
+		rem = n % 10;
+		num[c] = rem + 48;
+		n = n / 10;
+		c++;
+	}
+	a = a + c;
+	c = c - 1;
+	while (c >= 0)
+	{
+		write(1, &num[c], 1);
+		c--;
+	}
+	return (a);
+}
--- _printf.c
+++ _printf.c
@@ -3,53 +3,14 @@
 #include <stdarg.h>
 #include <unistd.h>
 /**
- * ptdi - print d and i
- * @num: number
- * Return: integer
- */
-int ptdi(int num)
-{
-	unsigned int rem, n, buffer;
-	int i, len = 0;
-	char s[11];
-
-	if (num < 0)
-	{
-		write(1, "-", 1);
-		n = -1 * num;
-		buffer = -1 * num;
-	}
-	else if (num == 0)
-	{
-		write(1, "0", 1), len = 0;
-		return (len);
-	}
-	else
-		n = num, buffer = num;
-	while (n != 0)
-	{
-		len++;
-		n /= 10;
-	}
-	for (i = 0; i < len; i++)
-	{
-		rem = buffer % 10;
-		buffer = buffer / 10;
-		s[i] = rem + '0';
-	}
-	s[len] = '\0';
-	for (i = len - 1; i >= 0; i--)
-		write(1, s + i, 1);
-	return (len);
-}
-/**
  * _printf - print
  * @format: str
  * Return: char
  */
 int _printf(const char *format, ...)
 {
-	int i, j, nr, count = 0, len = 0;
+	int len = 0;
+	unsigned int i, c = 0;
 	char fc;
 	char *fs;
 	va_list ptr;
@@ -58,32 +19,24 @@ int _printf(const char *format, ...)
 	for (i = 0; format != NULL && format[i] != '\0'; i++)
 	{
 		if (format[i] == '%' && format[i + 1] == 'c')
-			fc = va_arg(ptr, int), write(1, &fc, 1), i++;
+			c = _printc(c, va_arg(ptr, nt)), i++;
 		if (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'i'))
 		{
-			nr = va_arg(ptr, int), j = ptdi(nr);
-			count = count + j, i++;
+			c = _printd(c, va_arg(ptr, int)), i++;
 		}
 		else if (format[i] == '%' && format[i + 1] == 's')
 		{
-			fs = va_arg(ptr, char *);
-			if (fs == NULL)
-				fs = "(null)";
-			for (len = 0; fs[len] != '\0'; len++)
-				continue;
-			write(1, fs, len);
-			count = count + len - 1, i++;
+			c = _prints(c, va_arg(ptr, char *)), i++;
 		}
 		else if (format[i] == '%' && format[i + 1] == '%')
-			write(1, "%", 1), i++;
+			write(1, "%", 1), i++, c++;
 		else if (format[i] == '%' && format[i + 1] == '\0')
 			continue;
 		else
 			write(1, &format[i], 1);
-		count++;
 	}
 	va_end(ptr);
 	if (count == 0)
 		exit(31);
-	return (count);
+	return (c);
 }
