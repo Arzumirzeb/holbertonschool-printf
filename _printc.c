35f2c6332ea76b8304d135ad01077dc395a22895
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2023-12-01 01:54:09 -0800

    man by uzeyir

--- _printc.c
+++ _printc.c
@@ -8,20 +8,7 @@
  */
 int _printc(unsigned int *a, char b)
 {
-	if (!b && b !='\0')
-	{
-		write(1, "(", 1);
-		write(1, "n", 1);
-		write(1, "u", 1);
-		write(1, "l", 1);
-		write(1, "l", 1);
-		write(1, ")", 1);
-	*a += 6;
-	}
-	else
-	{
-		write(1, &b, 1);
-		(*a)++;
-	}
+	write(1, &b, 1);
+	(*a)++;
 	return (*a);
 }
